3e740573dbb62a1a9e5a34e147f1ae7928201b04
FGCA <fgca@outlook.es>
2024-02-25 14:15:16 -0600

    Creación de ejercicio 00 del Roadmap hecho en Java por GitFGCA

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/GitFGCA.java
@@ -0,0 +1,15 @@
+// Retos de programación no. 00 
+/* Esto es un comentario */
+
+/* Esto es un comentario largo
+ * 
+ * largo...
+ * 
+ * largo...
+ * 
+ */
+
+ /* URL de JAVA:                https://www.java.com/es/
+    URL de Desarrollo en JAVA:  https://dev.java/
+    URL tutoriales de JAVA:     https://docs.oracle.com/javase/tutorial/
+*/
